c9a1e5d0672db80dffae9ddbf228109900fe51b3
Karim Hamed <karimhamed947@gmail.com>
2025-05-07 21:31:04 +0300

    Initial Commit

---
+++ TabBarApp/TabBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarAppApp.swift
+//  TabBarApp
+//
+//  Created by Karim Hamed  on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TabBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
